6925fd1f853707fd5a29e1cf0400281c3e96d59f
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-01-27 17:49:43 -0300

    Projeto concluido

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -4,12 +4,15 @@ public class Curso extends Conteudo{
 
     private int cargaHoraria;
 
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+        super(titulo, descricao);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+
     @Override
     public double calcularXp() {
         return XP_PADRAO * cargaHoraria;
-    }
-
-    public Curso() {
     }
 
 
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -6,13 +6,16 @@ public class Mentoria extends Conteudo{
 
     private LocalDate data;
 
+    public Mentoria(String titulo, String descricao, LocalDate data) {
+        super(titulo, descricao);
+        this.data = data;
+    }
+
     @Override
     public double calcularXp() {
         return XP_PADRAO + 20d;
     }
 
-    public Mentoria() {
-    }
 
     public LocalDate getData() {
         return data;
